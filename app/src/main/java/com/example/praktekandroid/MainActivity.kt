e5cfff621bf1c9ac07b3f4fd29059b3468352570
Jidan0410 <81141547+Jidan0410@users.noreply.github.com>
2021-11-06 21:44:53 +0800

    Menghubungkan Kotlin dengan Komponen

--- app/src/main/java/com/example/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/example/praktekandroid/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.praktekandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val phoneEditText = findViewById<EditText>(R.id.phoneEditText)
+        val tglEditText = findViewById<EditText>(R.id.tglEditText)
+        val jkEditText = findViewById<EditText>(R.id.jkEditText)
+
+
+        simpanButton.setOnClickListener {
+            namaEditText.setText("")
+            emailEditText.setText("")
+            phoneEditText.setText("")
+            tglEditText.setText("")
+            jkEditText.setText("")
+        }
+
+        batalButton.setOnClickListener {
+            finish()
+        }
     }
 }
